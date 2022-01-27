468c6dc4fe90b3b8c1e5a55483ef7d81134519bc
Jae-Soon Kwon <rnjswotns12@naver.com>
2022-01-27 16:22:23 +0900

    Clustering(K-means)

---
+++ k-means/k-means.py
@@ -0,0 +1,71 @@
+# make dataset
+import numpy as np
+import matplotlib.pyplot as plt
+
+np.random.seed(1)
+N = 100
+K = 3
+T3 = np.zeros((N, 3), dtype=np.uint8)
+X = np.zeros((N, 2))
+X_range0 = [-3, 3]
+X_range1 = [-3 ,3]
+X_col = ['cornflowerblue', 'black', 'white']
+Mu = np.array([[-.5, -.5], [.5, 1.0], [1, -.5]])
+Sig = np.array([[.7, .7], [.8, .3], [.3, .8]])
+Pi = np.array([0.4, 0.8, 1])
+for n in range(N):
+    wk = np.random.rand()
+    for k in range(K):
+        if wk < Pi[k]:
+            T3[n, k] = 1
+            break
+    for k in range(2):
+        X[n, k] = (np.random.randn() * Sig[T3[n, :] == 1, k] + Mu[T3[n, :] == 1, k])
+
+Mu = np.array([[-2, 1], [-2, 0], [-2, -1]])
+R = np.c_[np.ones((N, 1), dtype=int), np.zeros((N, 2), dtype=int)]
+
+# show_data
+def show_prm(x, r, mu, col):
+    for k in range(K):
+        # 데이터 분표 묘사
+        plt.plot(x[r[:, k] == 1, 0], x[r[:, k] == 1, 1], marker='o',
+                  markerfacecolor=X_col[k], markeredgecolor='k', markersize=6, alpha=0.5, linestyle='none')
+        # 데이터 평균 표시
+        plt.plot(mu[k, 0], mu[k, 1], marker="*", markerfacecolor=X_col[k], markersize=15, markeredgecolor='k', markeredgewidth=1)
+        plt.xlim(X_range0)
+        plt.ylim(X_range1)
+        plt.grid(True)
+
+# (x0-u0)^2 + (x1-u1)^2 로 가장 가까운 거리 구하여 R 입력
+def step1_kmeans(x0, x1, mu):
+    N = len(x0)
+    r = np.zeros((N, K))
+    for n in range(N):
+        wk = np.zeros(K)
+        for k in range(K):
+            wk[k] = (x0[n] - mu[k, 0]) ** 2 + (x1[n] - mu[k, 1]) ** 2
+        r[n, np.argmin(wk)] = 1
+    return r        
+
+# Mu 결정
+def step2_kmeans(x0, x1, r):
+    mu = np.zeros((K, 2))
+    for k in range(K):
+        mu[k, 0] = np.sum(r[:, k] * x0) / np.sum(r[:, k])
+        mu[k, 1] = np.sum(r[:, k] * x1) / np.sum(r[:, k])
+    return mu
+
+# 안정화 위해 k-means 6회 반복
+plt.figure(1, figsize=(10, 6.5))
+Mu = np.array([[-2, 1], [-2, 0], [-2, -1]])
+max_it = 6
+for it in range(0, max_it):
+    plt.subplot(2, 3, it + 1)
+    R = step1_kmeans(X[:, 0], X[:, 1], Mu)
+    show_prm(X, R, Mu, X_col)
+    plt.title("{0:d}".format(it + 1))
+    plt.xticks(range(X_range0[0], X_range0[1]), "")
+    plt.yticks(range(X_range1[0], X_range1[1]), "")
+    Mu = step2_kmeans(X[:, 0], X[:, 1], R)
+plt.show()
