4c826b38f1610a8ad69b27ca4bb525d169414934
Jae-Soon Kwon <rnjswotns12@naver.com>
2022-01-27 14:52:08 +0900

    updata mnist

--- DeepLearning/mnist.py
+++ DeepLearning/mnist.py
@@ -1,6 +1,103 @@
-import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+from keras.datasets import mnist
+import numpy as np
+import matplotlib.pyplot as plt
+from keras.utils import np_utils
 
-import tensorflow as tf
+# get datasets
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-(x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
+# data pretreatment
+x_train = x_train.reshape(60000, 784)
+x_train = x_train.astype('float32')
+x_train = x_train / 255
+num_classes = 10
+y_train = np_utils.to_categorical(y_train, num_classes)
+x_test = x_test.reshape(10000, 784)
+x_test = x_test.astype('float32')
+x_test = x_test / 255
+y_test = np_utils.to_categorical(y_test, num_classes) # 1 - OF - K 부호화법
+
+# set network
+np.random.seed(1)
+from keras.models import Sequential
+from keras.layers import Dense, Activation
+from tensorflow.keras.optimizers import Adam
+
+model = Sequential()
+model.add(Dense(16, input_dim=784, activation='relu'))# 중간층
+model.add(Dense(10, activation='softmax')) # 출력층
+model.compile(loss='categorical_crossentropy', optimizer=Adam(), metrics=['accuracy'])
+
+# show loss and accuracy
+import time
+
+startTime = time.time()
+history = model.fit(x_train, y_train, epochs=10, batch_size=1000, verbose=1, validation_data=(x_test, y_test))
+score = model.evaluate(x_test, y_test, verbose=0)
+print("Test loss:", score[0])
+print("Test accuracy:", score[1])
+print("Computation time:{0:.3f} sec".format(time.time() - startTime))
+
+# show graph
+plt.figure(figsize=(10, 4))
+plt.subplots_adjust(wspace=0.5)
+
+plt.subplot(1, 2, 1)
+plt.plot(history.history['loss'], label='training', color='black')
+plt.plot(history.history['val_loss'], label='test', color='cornflowerblue')
+plt.ylim(0, 10)
+plt.legend()
+plt.grid()
+plt.xlabel('epoch')
+plt.ylabel('loss')
+
+plt.subplot(1, 2, 2)
+plt.plot(history.history['accuracy'], label='training', color='black')
+plt.plot(history.history['val_accuracy'], label='test', color='cornflowerblue')
+plt.ylim(0, 1)
+plt.legend()
+plt.grid()
+plt.xlabel('epoch')
+plt.ylabel('acc')
+plt.show()
+
+# show dataset
+def show_prediction():
+    n_show = 96
+    y = model.predict(x_test) # 전체 모델의 출력
+    plt.figure(2, figsize=(12, 8))
+    plt.gray()
+    for i in range(n_show):
+        plt.subplot(8, 12, i + 1)
+        x = x_test[i, :]
+        x = x.reshape(28, 28)
+        plt.pcolor(1 - x)
+        wk = y[i, :]
+        prediction = np.argmax(wk) # 정답
+        plt.text(22, 25.5, "%d" % prediction, fontsize=12)
+        if prediction != np.argmax(y_test[i, :]):
+            plt.plot([0, 27], [1, 1], color='cornflowerblue', linewidth=5)
+        plt.xlim(0, 27)
+        plt.ylim(27, 0)
+        plt.xticks([], "")
+        plt.yticks([], "")
+
+show_prediction()
+plt.show()
+
+# show weight
+w = model.layers[0].get_weights()[0] # 중간층 가중치 얻기
+plt.figure(1, figsize=(12, 3))
+plt.gray()
+plt.subplots_adjust(wspace=0.35, hspace=0.5)
+for i in range(16):
+    plt.subplot(2, 8, i + 1)
+    w1 = w[:, i]
+    w1 = w1.reshape(28, 28)
+    plt.pcolor(-w1)
+    plt.xlim(0, 27)
+    plt.ylim(27, 0)
+    plt.xticks([], "")
+    plt.yticks([], "")
+    plt.title("%d" %i)
+plt.show()
